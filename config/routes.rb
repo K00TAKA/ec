1a56e0e439e5e6ab0ff06a2188c33fd74fc66746
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-17 13:05:25 +0000

    [stop]routes大枠

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,14 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    resources :items, :genres, :customers, :orders, :order_details
+  end
+  
+  namespace :customers do
+    resources :items, :cart_items, :orders, :order_details, :addresses
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
