d5016c16d3076d61a7f2c1ab80d6e004b74f16b3
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-18 05:44:23 +0000

    [fix]routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,12 @@ Rails.application.routes.draw do
     resources :items, :genres, :customers, :orders, :order_details
   end
   
-  namespace :customer do
-    resources :items, :cart_items, :orders, :order_details, :addresses
-  end
+  resources :items
+  resources :cart_items
+  resources :orders
+  resources :order_details
+  resources :addresses
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
