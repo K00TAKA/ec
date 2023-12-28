704be51d25385b3cbe650b6826292c9015d19b04
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-28 15:06:32 +0000

    [finish]admin genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,3 +1,40 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
+  
+  def create
+  @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "You have created genre successfully."
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+    
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "You have updated genre successfully."
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  
+  def item_status
+    if is_active == true
+      "販売中"
+    else
+      "販売停止中"
+    end
+  end
+  
 end
