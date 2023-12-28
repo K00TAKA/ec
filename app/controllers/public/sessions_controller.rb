dd6dc0846123350510ff0478bc1f624f8e55629e
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-28 03:15:16 +0000

    [start]customer 退会ステータス処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -38,21 +38,24 @@ class Public::SessionsController < Devise::SessionsController
 
 
   # # If you have extra params to permit, append them to the sanitizer.
+
+  
+  private
+  
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
   
-  # private
-  
-  # def customer_state
-  #   # [処理内容1]　入力されたemailからアカウント1件取得
-  #   customer = Customer.find_by(email: params[:customer][:email])
-  #   # [処理内容2]　アカウントを取得できなかった場合、このメソッドを終了する
-  #   return if customer.nil?
-  #   # [処理内容3]　取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
-  #   return unless customer.valid_password?(params[:customer][:password])
-  #   # 【処理内容4】 アクティブでない会員に対する処理
-  #   # is_active(会員ステータス)カラムの中身に応じて処理を分岐させることでアクティブでない（退会している）場合の処理を実装することができます。
-  # end
+  def customer_state
+    # [処理内容1]　入力されたemailからアカウント1件取得
+    customer = Customer.find_by(email: params[:customer][:email])
+    # [処理内容2]　アカウントを取得できなかった場合、このメソッドを終了する
+    return if customer.nil?
+    # [処理内容3]　取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless customer.valid_password?(params[:customer][:password])
+    # 【処理内容4】 アクティブでない会員に対する処理
+    customer.update(is_deleted: true)
+    # 今回、会員情報を削除せず、is_deletedを使ってステータス管理する（trueの時退会、falseの時未退会）
+  end
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 end
