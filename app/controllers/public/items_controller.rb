00df93202b642493f4d2f2174569fcf3742d3835
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-30 15:38:25 +0000

    [stop]customer item画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,12 +3,14 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
     @genres = Genre.all
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
   
   def show
     @item = Item.find(params[:id])
     @genres = Genre.all
+    @orders = Order.new
   end
-  
+
+
 end
