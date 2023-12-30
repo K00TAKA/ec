52dae4adc3f79ccffe02fc4d785d8bcdad5a0a87
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-30 14:58:16 +0000

    [start]admin itemname link化

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,14 @@
 class Public::ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
 end
