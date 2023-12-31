45976100cf96fd658432a2bdccae7434d760f3c9
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2023-12-31 18:47:22 +0000

    [stop]アソシエーションcart_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   # ActiveStorageを使って画像を持たせる
   has_one_attached :image
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
   
   def item_status
     if is_active == true
