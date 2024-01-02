be2b59d261b2c728e938bb516149c64a845a5833
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2024-01-02 19:08:07 +0000

    [stop]cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,3 +1,19 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
+  
+  def index
+      @cart_item = CartItem.new
+      @cart_items = CartItem.all
+  end
+  
+  def destroy_all
+    CartItem.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_back(fallback_location: root_path)
+  end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,9 +8,29 @@ class Public::ItemsController < ApplicationController
   
   def show
     @item = Item.find(params[:id])
+    @cart_item_new = CartItem.new
     @genres = Genre.all
-    @orders = Order.new
+    @order = Order.new
   end
-
-
+  
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end
