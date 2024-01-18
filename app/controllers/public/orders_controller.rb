f644a857333e009b61abf236c3f5d5acca0df2e6
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2024-01-18 05:24:22 +0000

    [fix]customer order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -30,7 +30,7 @@ class Public::OrdersController < ApplicationController
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
     when "registered_address"
-      Addresse.find(params[:order][:registered_address_id])
+      Address.find(params[:order][:registered_address_id])
       selected = Address.find(params[:order][:registered_address_id])
       @order.postal_code = selected.postal_code
       @order.address = selected.address
@@ -42,35 +42,32 @@ class Public::OrdersController < ApplicationController
     end
     
     if @order.save
-      if @order.status == 0
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 0)
-        end
-      else
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 1)
-        end
+      making_status_mapping = { 0=>0, 1=>1, 2=>2, 3=>3, 4=>4 }
+      making_status = making_status_mapping[@order.status]
+      
+      @cart_items.each do |cart_item|
+        OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price_with_tax: cart_item.item.price, amount: cart_item.amount, making_status: making_status)
       end
+      
       @cart_items.destroy_all
       redirect_to complete_orders_path
     else
-      render :items
+      render :new
     end
   end
 
   def confirm
+    @order = Order.new(order_params)
+    # @order.postal_code = current_customer.postal_code
+    # @order.address = current_customer.address
+    # @order.name = current_customer.first_name + current_customer.last_name
     @cart_items = CartItem.where(customer_id: current_customer.id)
-     #送料800円固定
     @postage = 800 
     @selected_payment_method = params[:order][:payment_method]
-  
-    #以下、商品合計額の計算
+    
     ary = []
-    @cart_items.each do |cart_item|
-      ary << cart_item.item.price*cart_item.amount
-    end
     @cart_items_price = ary.sum
-        
+      
     @total_price = @postage + @cart_items_price
     @address_type = params[:order][:address_type]
     case @address_type
@@ -84,12 +81,23 @@ class Public::OrdersController < ApplicationController
         render :new
       end
     when "new_address"
-      unless params[:order][:new_postal_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
-        @selected_address = params[:order][:new_postal_code] + " " + params[:order][:new_address] + " " + params[:order][:new_name]
+      unless params[:order][:postal_code] == "" && params[:order][:address] == "" && params[:order][:name] == ""
+        @selected_address = params[:order][:postal_code] + params[:order][:address] + params[:order][:name]
       else
         render :new
       end
-    end     
+    end
+  end
+  
+  def index
+    @orders = Order.all
+  end
+
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   
   enum payment_method: { credit_card: 0, bank: 1 }
-  # ↓これはどこに記載するべき？ここに記載しておくとエラー出る
-  enum status: { waiting: 0, confirmation: 1, making: 2, preparation: 3, send: 4 }
+  # enum status: { waiting: 0, confirmation: 1, making: 2, preparation: 3, send: 4 }
   
 end
